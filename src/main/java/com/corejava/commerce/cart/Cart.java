10680ed26f778f14a84cac8f69f057c18b2c8ba7
karpuz <143242891+sena-efkk@users.noreply.github.com>
2026-06-30 02:37:15 +0300

    fix sprint3

--- src/main/java/com/corejava/commerce/cart/Cart.java
+++ src/main/java/com/corejava/commerce/cart/Cart.java
@@ -1,4 +1,64 @@
 package com.corejava.commerce.cart;
 
+import com.corejava.commerce.catalog.Product;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Cart {
+
+    private List<CartItem> items;
+
+    public Cart(){
+        this.items = new ArrayList<>();
+
+    }
+
+    public void addProduct(Product product, int newquantity){
+        //sepete urun ekleme etodu
+        CartItem ci =new CartItem(product,newquantity);
+        for(CartItem c:items){
+            if(c.getProduct().hasEnoughStock(newquantity)){
+                items.add(ci);
+
+            }
+            throw new IllegalArgumentException("Stok yetersiz.");
+
+        }
+
+    }
+
+    //sepetin tamamının listesi isteniuor
+    public List<CartItem> getItems(){
+        //Sepetteki satırları görmek istiyorsam Cart.getItems() kullanmalıyım. -> bu ne demek anlamadım?
+        List<CartItem> listesi= new ArrayList<>();
+        for (CartItem i :items){
+            listesi.add(i);
+        }
+        return listesi;
+    }
+
+    public BigDecimal getTotalPrice(){
+    //Sepetteki tüm ürünlerin toplam fiyatını hesapla.
+        BigDecimal total;
+        for(CartItem item:items){
+
+            //Sepetin genel toplamını istiyorsam Cart.getTotalPrice() kullanmalıyım.-> yazamadım syntaxını
+        }
+    return null;//ne dondurcemi bilmediğim için şuan boş bıraktım bilerek yani.
+    }
+
+    public boolean isEmpty(){
+        //Sepette hiç ürün var mı?
+
+        return items.isEmpty();
+    }
+
+    public void clear(){
+        items.clear();
+    }
+
 }
--- src/main/java/com/corejava/commerce/cart/CartItem.java
+++ src/main/java/com/corejava/commerce/cart/CartItem.java
@@ -1,4 +1,52 @@
 package com.corejava.commerce.cart;
 
+import com.corejava.commerce.catalog.Product;
+import com.corejava.commerce.catalog.ProductCatalogService;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+//CartItem: sepetteki tek bir satırı temsil eder.
 public class CartItem {
+
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product,int quantity){
+        if(product==null){
+            throw new IllegalArgumentException("Ürün boş olamaz.");
+
+        }
+        if(quantity<= 0 || product.hasEnoughStock(quantity)){
+            throw new IllegalArgumentException("miktar 0 veya negatif olamaz.");
+
+        }
+        this.product=product;
+        this.quantity=quantity;
+
+    }
+
+    public Product getProduct(){
+        return this.product;
+    }
+
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public void increaseQuantity(int amount){
+        this.quantity=quantity+amount;
+    }
+
+    public void decreaseQuantity(int amount){
+        this.quantity=quantity-amount;
+
+    }
+
+    public BigDecimal getLineTotal(){
+        return product.getPrice().multiply(BigDecimal.valueOf(quantity));
+    }
+
+
+
 }
